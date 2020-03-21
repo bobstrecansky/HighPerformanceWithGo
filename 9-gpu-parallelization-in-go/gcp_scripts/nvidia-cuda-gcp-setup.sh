a37a573f7bfb11fc7992b7ad4c1ee949d5ba9f6e
Bob Strecansky <bob.strecansky@gmail.com>
2020-03-21 16:05:16 -0400

    GCP setup script updates

--- 9-gpu-parallelization-in-go/gcp_scripts/nvidia-cuda-gcp-setup.sh
+++ 9-gpu-parallelization-in-go/gcp_scripts/nvidia-cuda-gcp-setup.sh
@@ -20,4 +20,5 @@ distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 apt-get -y update && sudo apt-get -y install nvidia-container-toolkit
+usermod -aG docker $USER
 systemctl restart docker
