854f29440ea78216ac8ef1e55b1a6b7e8c138258
Bob Strecansky <bob.strecansky@gmail.com>
2020-03-21 15:42:55 -0400

    GCP setup script updates

--- 9-gpu-parallelization-in-go/gcp_scripts/nvidia-cuda-gcp-setup.sh
+++ 9-gpu-parallelization-in-go/gcp_scripts/nvidia-cuda-gcp-setup.sh
@@ -2,22 +2,22 @@
 
 # Install the CUDA driver
 curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo apt-get -y update && sudo apt-get -y install cuda
+dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+apt-get -y update && sudo apt-get -y install cuda
 
 # Install Docker CE
-sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt-get remove docker docker-engine docker.io containerd runc
+apt-get update
+apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get -y update
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get -y update
+apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # Install nvidia-docker
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get -y update && sudo apt-get -y install nvidia-container-toolkit
-sudo systemctl restart docker
+apt-get -y update && sudo apt-get -y install nvidia-container-toolkit
+systemctl restart docker
